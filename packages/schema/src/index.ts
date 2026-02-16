e716fd7b16160b8de3e67741c4157dd097e375b0
workingwithali <alirashid2020e@gmail.com>
2026-02-16 13:56:45 +0500

    remove all file

--- packages/schema/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const UserSchema = z.object({
-    id: z.string(),
-    email: z.string().email(),
-    name: z.string().optional(),
-});
-
-export type User = z.infer<typeof UserSchema>;
